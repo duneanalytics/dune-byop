c942869625f3249cead89a7f5c4035827ef8e64b
agaperste <agapehumilduro@gmail.com>
2024-02-01 17:53:05 -0500

    grabbing the from to from txns, timestamp from blocks, and joining it together with the logs info.. for all events in this contract, and now uploading correctly for 800 blocks

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ dotenv.config();
 
 export const publicClient = createPublicClient({
     chain: mainnet,
-    transport: http()
+    transport: http('https://eth-mainnet.g.alchemy.com/v2/RyMo60-cLKdpoQxbAiCUt3m9pQjPVnX4')
 })
 
 async function main() {
@@ -17,68 +17,108 @@ async function main() {
      * 4. call getBlock to grab timestamp, then convert to block_time and block_date 
      */
 
-    const logs = await publicClient.getLogs({
+    const currentBlockNumber = 19073790n //await publicClient.getBlockNumber()
+
+    let fromBlock = 19072790n;
+    let toBlock = fromBlock + 799n; // max range is 800
+
+    const logs = [];
+
+    // while (toBlock <= currentBlockNumber) {
+    console.log(`Fetching logs from block ${fromBlock} to ${toBlock}`);
+    const result = await publicClient.getLogs({
         address: '0xb4e16d0168e52d35cacd2c6185b44281ec28c9dc',
         events: parseAbi([
             'event Swap(address indexed sender, uint256 amount0In, uint256 amount1In, uint256 amount0Out, uint256 amount1Out, address indexed to)',
-            'event Sync(uint112 reserve0, uint112 reserve1)'
+            'event Sync(uint112 reserve0, uint112 reserve1)',
+            'event Approval(address indexed owner, address indexed spender, uint256 value)',
+            'event Burn(address indexed sender, uint256 amount0, uint256 amount1, address indexed to)',
+            'event Mint(address indexed sender, uint256 amount0, uint256 amount1)',
+            'event Transfer(address indexed from, address indexed to, uint256 value)'
+
         ]),
-        // event: parseAbiItem('event Swap(address indexed sender, uint256 amount0In, uint256 amount1In, uint256 amount0Out, uint256 amount1Out, address indexed to)'),
-        blockHash: '0x6bd38cd96a967ec730acbbccc12564c769ccd03fcbeb702b3ef646044625a0bd'
-    })
+        fromBlock,
+        toBlock
+    });
+    logs.push(...result);
+    // fromBlock = toBlock + 1n;
+    // toBlock = fromBlock + 799n; //TODO someone check my math here plz
+    // }
 
-    console.log(logs)
+    // console.log(logs)
 
-    const transaction = await publicClient.getTransaction({
-        hash: '0x40396609d599ca124517e6ce2b6bbaa5f124421857ce97b9bba0d5bc2baaa5c8',
-    })
+    const transactionHashList = logs.map(log => log.transactionHash);
+    const blockNumberList = logs.map(log => log.blockNumber);
 
-    console.log(`tx_from = ${transaction.from}`)
-    console.log(`tx_to = ${transaction.to}`)
+    const transactions: { transaction_hash: string, transaction_from: string, transaction_to: string }[] = [];
+    for (let i = 0; i < transactionHashList.length; i++) {
+        const transaction = await publicClient.getTransaction({
+            hash: transactionHashList[i],
+        });
 
-    const block = await publicClient.getBlock({
-        blockNumber: 19072790n,
-    })
+        transactions.push({
+            transaction_hash: transaction.hash,
+            transaction_from: transaction.from ?? '',
+            transaction_to: transaction.to ?? ''
+        });
+    }
 
-    console.log(`block_timestamp_raw = ${block.timestamp}`)
+    const blocks: { block_number: BigInt, block_time: string, block_date: string }[] = [];
+    for (let i = 0; i < blockNumberList.length; i++) {
+        const block = await publicClient.getBlock({
+            blockNumber: blockNumberList[i],
+        });
 
-    const timestampInSeconds = Number(block.timestamp);
-    const block_time = new Date(timestampInSeconds * 1000).toISOString().replace('T', ' ').replace(/\.\d+Z/, '');
-    console.log(`block_time = ${block_time}`);
+        const timestampInSeconds = Number(block.timestamp);
+        const block_time = new Date(timestampInSeconds * 1000).toISOString().replace('T', ' ').replace(/\.\d+Z/, '');
+        const block_date = block_time.substring(0, 10);
 
-    const block_date = block_time.substring(0, 10);
-    console.log(`block_date = ${block_date}`);
+        blocks.push({
+            block_number: block.number,
+            block_time: block_time,
+            block_date: block_date
+        });
+    }
+
+    const fullTable = logs.map(log => {
+        const matchingTransaction = transactions.find(transaction => transaction.transaction_hash === log.transactionHash);
+        const matchingBlock = blocks.find(block => block.block_number === log.blockNumber);
+
+        return `${log.blockHash},${log.blockNumber.toString()},${matchingBlock?.block_time},${matchingBlock?.block_date},${log.address},${log.eventName},${log.transactionHash},${matchingTransaction ? matchingTransaction.transaction_from : ''},${matchingTransaction ? matchingTransaction.transaction_to : ''},${log.transactionIndex.toString()},${log.logIndex.toString()},${log.topics[0] || ''},${log.topics.length > 1 ? log.topics[1] : ''},${log.topics.length > 2 ? log.topics[2] : ''},${log.topics.length > 3 ? log.topics[3] : ''},${log.data},${log.removed.toString()}`;
+    }).join('\n');
+
+    // console.log(fullTable)
 
 
     // CSV header
     const csvHeader = 'block_hash,block_number,block_time,block_date,contract_address,event_name,tx_hash,tx_from,tx_to,tx_index,log_index,topic0,topic1,topic2,topic3,data,removed\n';
 
-    // Convert logs to CSV
-    const csvRows = logs.map(log => {
-        const topics = log.topics as string[];
+    // // Convert logs to CSV
+    // const csvRows = logs.map(log => {
+    //     const topics = log.topics as string[];
 
-        return [
-            log.blockHash,
-            log.blockNumber.toString(),
-            block_time,
-            block_date,
-            log.address,
-            log.eventName,
-            log.transactionHash,
-            transaction.from,
-            transaction.to,
-            log.transactionIndex.toString(),
-            log.logIndex.toString(),
-            topics[0] || '',
-            topics.length > 1 ? topics[1] : null,
-            topics.length > 2 ? topics[2] : null,
-            topics.length > 3 ? topics[3] : null,
-            log.data,
-            log.removed.toString()
-        ].join(',');
-    }).join('\n');
+    //     return [
+    //         log.blockHash,
+    //         log.blockNumber.toString(),
+    //         block_time,
+    //         block_date,
+    //         log.address,
+    //         log.eventName,
+    //         log.transactionHash,
+    //         transaction.from ?? '',
+    //         transaction.to ?? '',
+    //         log.transactionIndex.toString(),
+    //         log.logIndex.toString(),
+    //         topics[0] || '',
+    //         topics.length > 1 ? topics[1] : null,
+    //         topics.length > 2 ? topics[2] : null,
+    //         topics.length > 3 ? topics[3] : null,
+    //         log.data,
+    //         log.removed.toString()
+    //     ].join(',');
+    // }).join('\n');
 
-    const csv = csvHeader + csvRows;
+    const csv = csvHeader + fullTable;
     console.log(csv);
 
     // Approximate size in bytes (assuming UTF-16, 2 bytes per character)
