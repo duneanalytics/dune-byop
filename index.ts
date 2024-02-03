0758c6c538c33b8f02a9306b254edad1c307065b
agaperste <agapehumilduro@gmail.com>
2024-02-03 13:06:26 -0500

    script to fetch all relevant logs info for all functions in a contract works for UniswapV3 ETH USDC 10 pool! Added error catching and retrys as well as saving the csv in local just in case

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 import { createPublicClient, http } from 'viem';
 import { base } from 'viem/chains';
-import * as fs from 'fs';
+const fs = require('fs');
 import * as dotenv from 'dotenv';
 
 dotenv.config();
@@ -18,12 +18,14 @@ async function main() {
      * This block gets all the logs or events for a particular contract
      * TODO refactor out so contract address & block number the contract was deployed at & CHUNK SIZE can be parameters
      */
+    const startTimeLogs: number = Date.now();
+
     const currentBlockNumber = await publicClient.getBlockNumber();
-    let deployedBlock: bigint = 3620407n;
+    let deployedBlock: bigint = 3620407n; // TODO: parameterize
     const logs: any[] = [];
     console.log(`Preparing to fetch logs for from ${deployedBlock} to ${currentBlockNumber}.`);
 
-    const MAX_RANGE = 9999n; //QuickNode has 10k blocks limit
+    const MAX_RANGE = 9999n; //// TODO: parameterize || QuickNode has 10k blocks limit
     const ranges: { fromBlock: bigint, toBlock: bigint }[] = [];
     for (let from = deployedBlock; from <= currentBlockNumber; from += MAX_RANGE + 1n) {
         const toBlock = BigInt(Math.min(Number(from) + Number(MAX_RANGE), Number(currentBlockNumber)));
@@ -31,7 +33,7 @@ async function main() {
     }
 
     const logPromises = ranges.map(range => publicClient.getLogs({
-        address: '0xd0b53D9277642d899DF5C87A3966A349A798F224',
+        address: '0xd0b53D9277642d899DF5C87A3966A349A798F224', // TODO: parameterize
         fromBlock: range.fromBlock,
         toBlock: range.toBlock
     }));
@@ -44,157 +46,195 @@ async function main() {
         console.error("Error calling getLogs:\n", error);
     }
 
-    // =============================================================================
-    /**
-     * Save all the logs for future debugging so we don't need to run it over and over again
-     */
-    const logFilePath = 'saved_logs.json';
-    const stringifiedLogs = logs.map(log => ({
-        ...log,
-        blockNumber: log.blockNumber.toString()
-    }));
-    fs.writeFileSync(logFilePath, JSON.stringify(stringifiedLogs, null, 2));
-    console.log(`Logs saved to ${logFilePath}`);
-
-    // =============================================================================
-    /**
-     * This block is for when starting with saved logs, mainly used to debug the getBlocks and getTransactions calls
-     */
-    // type Log = {
-    //     address: string;
-    //     blockHash: string;
-    //     blockNumber: string;
-    //     data: string;
-    //     logIndex: number;
-    //     removed: boolean;
-    //     topics: string[];
-    //     transactionHash: string;
-    //     transactionIndex: number;
-    // };
-
-    // const logs: Log[] = require('./saved_logs.json');
-
+    const endTimeLogs: number = Date.now();
+    console.log(`GetLogs execution time: ${(endTimeLogs - startTimeLogs) / 1000} seconds`);
     // =================================================================
     /**
      * Fetching block time from
      */
 
+    const startTimeBlocks: number = Date.now();
 
-    // STOPPING POINT: switch to quicknode, so test that it still works, then potentially get the getBlockReceipt function for base
-    // but anyways test out the parallelization with that 
+    const blockNumberList = logs.map(log => log.blockNumber);
+    console.log(`Number of blocks to get info on: ${blockNumberList.length}`);
+    console.log('First 10 elements in blockNumberList:', blockNumberList.slice(0, 10));
 
-    //     const blockNumberList = logs.map(log => log.blockNumber);
-    //     console.log(`Number of blocks to get info on: ${blockNumberList.length}`);
+    const BLOCK_CHUNK_SIZE = 5000; // TODO: parameterize
 
-    //     const CHUNK_SIZE = 80; // Number of blocks to process at a time, this is to avoid rate limiting
-    //     const blocks: { block_number: bigint, block_time: string, block_date: string }[] = [];
-    //     for (let i = 0; i < blockNumberList.length; i += CHUNK_SIZE) {
-    //         const blockNumberChunk = blockNumberList.slice(i, i + CHUNK_SIZE);
-    //         console.log(`Fetching blocks from ${blockNumberChunk[0]} to ${blockNumberChunk[blockNumberChunk.length - 1]}`);
+    const blocks: { block_number: bigint, block_time: string, block_date: string }[] = [];
+    let leftBlocks: number[] = [...blockNumberList]; // Initially set to all blocks
 
-    //         // Collect an array of promises for this chunk
-    //         const blockPromises = blockNumberChunk.map(blockNumber => publicClient.getBlock({
-    //             blockNumber: BigInt(blockNumber), // TODO: remove BigInt casting after testing
-    //         }));
+    const fetchBlocks = async (blockNumbers: number[]) => {
+        let newFailedBlocks: number[] = [];
 
-    //         // Resolve all promises in this chunk
-    //         const results = await Promise.all(blockPromises);
+        for (let i = 0; i < blockNumbers.length; i += BLOCK_CHUNK_SIZE) {
+            const blockNumberChunk = blockNumbers.slice(i, i + BLOCK_CHUNK_SIZE);
+            console.log(`Fetching blocks from ${blockNumberChunk[0]} to ${blockNumberChunk[blockNumberChunk.length - 1]}`);
 
-    //         // Process results
-    //         const blockDetails = results.map(block => {
-    //             const timestampInSeconds = Number(block.timestamp);
-    //             const block_time = new Date(timestampInSeconds * 1000).toISOString().replace('T', ' ').replace(/\.\d+Z/, '');
-    //             const block_date = block_time.substring(0, 10);
+            const blockPromises = blockNumberChunk.map(blockNumber => publicClient.getBlock({
+                blockNumber: BigInt(blockNumber),
+            }).catch(error => {
+                // Check if error.body and error.body.params are defined
+                if (error.body && error.body.params && error.body.params.length > 0) {
+                    const failedBlockNumber = parseInt(error.body.params[0], 16);
+                    newFailedBlocks.push(blockNumber);
+                    console.error(`Error fetching block ${failedBlockNumber}: ${error}`);
+                } else {
+                    // Handle the case where error.body or error.body.params is undefined
+                    newFailedBlocks.push(blockNumber);
+                    console.error(`Unexpected error fetching block ${blockNumber}: ${error}`);
+                }
+                return null; // Return null for failed requests
+            }));
 
-    //             return {
-    //                 block_number: block.number,
-    //                 block_time: block_time,
-    //                 block_date: block_date
-    //             };
-    //         });
+            const results = await Promise.all(blockPromises);
+            results.forEach(block => {
+                if (block) {
+                    const timestampInSeconds = Number(block.timestamp);
+                    const block_time = new Date(timestampInSeconds * 1000).toISOString().replace('T', ' ').replace(/\.\d+Z/, '');
+                    const block_date = block_time.substring(0, 10);
 
-    //         blocks.push(...blockDetails);
-    //     }
+                    blocks.push({
+                        block_number: block.number,
+                        block_time,
+                        block_date
+                    });
+                }
+            });
+        }
 
-    // =============================================================================
-    /**
-     * Save all the blocks for future debugging so we don't need to run it over and over again
-     */
-    // const blockFilePath = 'test_blocks.json';
-    // const blocksJson = JSON.stringify(blocks.map(block => ({
-    //     block_number: block.block_number.toString(),
-    //     block_time: block.block_time,
-    //     block_date: block.block_date
-    // })), null, 2);
-    // fs.writeFileSync(blockFilePath, blocksJson);
-    // console.log(`Logs saved to ${blockFilePath}`);
+        return newFailedBlocks;
+    };
+
+    // Keep retrying until there are no more failed blocks
+    while (leftBlocks.length > 0) {
+        console.log(`Trying/Retrying ${leftBlocks.length} left blocks.`);
+        leftBlocks = await fetchBlocks(leftBlocks);
+    }
+
+    // Deduplicate blocks
+    const uniqueBlocks = Array.from(new Map(blocks.map(block => [block.block_number, block])).values());
+    console.log('All blocks have been fetched and deduplicated.');
+
+
+    const endTimeBlocks: number = Date.now();
+    console.log(`GetBlocks execution time: ${(endTimeBlocks - startTimeBlocks) / 60000} minutes`);
 
     // =================================================================
     /**
      * Fetching txn from and to
      */
-    // const transactionHashList = logs.map(log => log.transactionHash);
-    // console.log(`Number of transactions to get info on: ${transactionHashList}`);
+    const startTimeTxns: number = Date.now();
 
-    // const transactions: { transaction_hash: string, transaction_from: string, transaction_to: string }[] = [];
-    // for (let i = 0; i < transactionHashList.length; i++) {
-    //     console.log(`Fetching transaction ${transactionHashList[i]}`);
-    //     const transaction = await publicClient.getTransaction({
-    //         hash: transactionHashList[i],
-    //     });
+    const transactionHashList = logs.map(log => log.transactionHash);
+    console.log(`Number of transactions to get info on: ${transactionHashList.length}`);
+    console.log('First 10 elements in transactionHashList:', transactionHashList.slice(0, 10));
 
-    //     transactions.push({
-    //         transaction_hash: transaction.hash,
-    //         transaction_from: transaction.from ?? '',
-    //         transaction_to: transaction.to ?? ''
-    //     });
-    // }
+    const TXN_CHUNK_SIZE = 5000; // TODO: parameterize
 
-    // console.log("Joining all together for the full table")
-    // const fullTable: string = logs.map((log: any) => {
-    //     const matchingTransaction = transactions.find((transaction: any) => transaction.transaction_hash === log.transactionHash);
-    //     const matchingBlock = blocks.find((block: any) => block.block_number === log.blockNumber);
+    const transactions: { transaction_hash: string, transaction_from: string, transaction_to: string }[] = [];
+    let leftTransactions: string[] = [...transactionHashList]; // Initially set to all transactions
 
-    //     return `${log.blockHash},${log.blockNumber.toString()},${matchingBlock?.block_time},${matchingBlock?.block_date},${log.address},${log.transactionHash},${matchingTransaction ? matchingTransaction.transaction_from : ''},${matchingTransaction ? matchingTransaction.transaction_to : ''},${log.transactionIndex.toString()},${log.logIndex.toString()},${log.topics[0] || ''},${log.topics.length > 1 ? log.topics[1] : ''},${log.topics.length > 2 ? log.topics[2] : ''},${log.topics.length > 3 ? log.topics[3] : ''},${log.data},${log.removed.toString()}`;
-    // }).join('\n');
+    const fetchTransactions = async (transactionHashes: string[]) => {
+        let newFailedTransactions: string[] = [];
 
-    // // CSV header
-    // const csvHeader: string = 'block_hash,block_number,block_time,block_date,contract_address,tx_hash,tx_from,tx_to,tx_index,log_index,topic0,topic1,topic2,topic3,data,removed\n';
+        for (let i = 0; i < transactionHashes.length; i += TXN_CHUNK_SIZE) {
+            const transactionHashChunk = transactionHashes.slice(i, i + TXN_CHUNK_SIZE);
+            console.log(`Fetching transactions for chunk starting from ${transactionHashChunk[0]}`);
 
-    // const csv: string = csvHeader + fullTable;
-    // console.log(csv);
+            const transactionPromises = transactionHashChunk.map(hash => publicClient.getTransaction({
+                hash: (hash.startsWith('0x') ? hash : `0x${hash}`) as `0x${string}`,
+            }).catch(error => {
+                newFailedTransactions.push(hash);
+                console.error(`Error fetching transaction ${hash}: ${error}`);
+                return null; // Return null for failed requests
+            }));
 
-    // // Approximate size in bytes (assuming UTF-16, 2 bytes per character)
-    // const sizeInBytes: number = new TextEncoder().encode(csv).length;
-    // const sizeInMegabytes: number = sizeInBytes / (1024 * 1024);
+            const results = await Promise.all(transactionPromises);
+            results.forEach(transaction => {
+                if (transaction) {
+                    transactions.push({
+                        transaction_hash: transaction.hash,
+                        transaction_from: transaction.from ?? '',
+                        transaction_to: transaction.to ?? ''
+                    });
+                }
+            });
+        }
 
-    // console.log(`CSV Size: ${sizeInMegabytes.toFixed(2)} MB`);
+        return newFailedTransactions;
+    };
 
-    // // Upload CSV to Dune
-    // const dune_api_key = process.env.DUNE_API_KEY as string; // Assuming the API key is stored in an environment variable
-    // const headers: Record<string, string> = {
-    //     'Content-Type': 'application/json'
-    // };
+    // Keep retrying until there are no more failed transactions
+    while (leftTransactions.length > 0) {
+        console.log(`Trying/Retrying ${leftTransactions.length} left transactions.`);
+        leftTransactions = await fetchTransactions(leftTransactions);
+    }
 
-    // if (dune_api_key) {
-    //     headers['X-DUNE-API-KEY'] = dune_api_key;
-    // }
+    console.log('All transactions have been fetched.');
 
-    // const options = {
-    //     method: 'POST',
-    //     headers: headers,
-    //     body: JSON.stringify({
-    //         data: csv,
-    //         table_name: "jackie_test_byop_base_all_events_raw",
-    //         is_private: true
-    //     })
-    // };
 
-    // console.log("Uploading to Dune...")
-    // fetch('https://api.dune.com/api/v1/table/upload/csv', options)
-    //     .then(response => response.json())
-    //     .then(response => console.log(response))
-    //     .catch(err => console.error(err));
+    const endTimeTxns: number = Date.now();
+    console.log(`GetTransactions execution time: ${(endTimeTxns - startTimeTxns) / 60000} minutes`);
+    // =================================================================
+    /**
+     * Joining all fetched info into one table
+     */
+
+    console.log("Joining all together for the full table")
+    // Create indexed structures
+    const blockIndex = new Map(uniqueBlocks.map(block => [block.block_number.toString(), block]));
+    const transactionIndex = new Map(transactions.map(transaction => [transaction.transaction_hash, transaction]));
+
+    const fullTable = logs.map(log => {
+        const matchingTransaction = transactionIndex.get(log.transactionHash);
+        const matchingBlock = blockIndex.get(log.blockNumber.toString());
+
+        return `${log.blockHash},${log.blockNumber.toString()},${matchingBlock?.block_time},${matchingBlock?.block_date},${log.address},${log.transactionHash},${matchingTransaction ? matchingTransaction.transaction_from : ''},${matchingTransaction ? matchingTransaction.transaction_to : ''},${log.transactionIndex.toString()},${log.logIndex.toString()},${log.topics[0] || ''},${log.topics.length > 1 ? log.topics[1] : ''},${log.topics.length > 2 ? log.topics[2] : ''},${log.topics.length > 3 ? log.topics[3] : ''},${log.data},${log.removed.toString()}`;
+    }).join('\n');
+
+    const csvHeader: string = 'block_hash,block_number,block_time,block_date,contract_address,tx_hash,tx_from,tx_to,tx_index,log_index,topic0,topic1,topic2,topic3,data,removed\n';
+
+    const csv: string = csvHeader + fullTable;
+    fs.writeFileSync('uniswapV3_ETH_USDC_10_logs.csv', csv);
+
+    const sizeInBytes: number = new TextEncoder().encode(csv).length;
+    const sizeInMegabytes: number = sizeInBytes / (1024 * 1024);
+    console.log(`CSV Size: ${sizeInMegabytes.toFixed(2)} MB`);
+
+    // =================================================================
+    /**
+     * Upload CSV to Dune
+     */
+    const dune_api_key = process.env.DUNE_API_KEY as string; // Assuming the API key is stored in an environment variable
+    const headers: Record<string, string> = {
+        'Content-Type': 'application/json'
+    };
+
+    if (dune_api_key) {
+        headers['X-DUNE-API-KEY'] = dune_api_key;
+    }
+
+    const options = {
+        method: 'POST',
+        headers: headers,
+        body: JSON.stringify({
+            data: csv,
+            table_name: "jackie_test_byop_base_all_events_raw",
+            is_private: true
+        })
+    };
+
+    console.log("Uploading to Dune...")
+    fetch('https://api.dune.com/api/v1/table/upload/csv', options)
+        .then(response => response.json())
+        .then(response => console.log(response))
+        .catch(err => console.error(err));
+
+
+    const end = new Date().getTime();
+    const minutesTaken = (end - startTimeLogs) / 60000;
+    console.log(`Total time taken for this process: ${minutesTaken} minutes`);
 }
 
 main().catch(console.error);
