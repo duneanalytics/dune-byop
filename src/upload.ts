b4c42be85cb3be91ea8caa4e4ccf2cb7d13ede29
agaperste <agapehumilduro@gmail.com>
2024-03-15 15:13:32 -0400

    adding append to upload -- first fetch for latest block already existing on the table, then appending only not uploaded block records

--- src/upload.ts
+++ src/upload.ts
@@ -1,8 +1,13 @@
 const fs = require('fs');
 import * as dotenv from 'dotenv';
 
+interface CreateQueryRequest {
+    name: string;
+    query_sql: string;
+}
+
 dotenv.config();
-const dune_api_key = process.env.DUNE_API_KEY as string;
+const dune_api_key = process.env.DUNE_API_KEY as string; 
 
 async function createLogsTable(duneTableNamespace: string, duneTableName: string, apiKey: string){
     /*
@@ -51,21 +56,156 @@ async function createLogsTable(duneTableNamespace: string, duneTableName: string
         
         if (!response.ok) {
             console.error("Error creating table:", json);
-            throw new Error(`Failed to create table: ${json.message || 'Unknown error'}`);
+            // throw new Error(`Failed to create table: ${json.error || 'Unknown error'}`); // TODO decide if we want to throw an error here, prolly not
         } else {
             console.log(`Table created: dune.${duneTableNamespace}.${duneTableName}`);
         }
     } catch (error) {
-        console.error("Error creating table:", error);
         throw error; // Rethrow the error to be caught by the caller
     }
     
 }
 
+
+async function createDuneQuery(apiKey: string, sql: string): Promise<number> {
+    const queryRequest: CreateQueryRequest = {name: 'temp', query_sql: sql};
+
+    console.log(`queryRequest: ${JSON.stringify(queryRequest)}`)
+    const options = {
+        method: 'POST',
+        headers: {
+            'X-DUNE-API-KEY': apiKey,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(queryRequest)
+    };
+
+    try {
+        const response = await fetch('https://api.dune.com/api/v1/query', options);
+        if (!response.ok) {
+            throw new Error(`Failed to create query: ${response.statusText}`);
+        }
+        const responseData = await response.json();
+        console.log('Query created:', responseData);
+        return responseData.query_id;
+    } catch (error) {
+        throw error;
+    }
+}
+
+async function fetchDuneQuery(apiKey: string, queryId: number): Promise<number> {
+    const executeEndpoint = `https://api.dune.com/api/v1/query/${queryId}/execute`;
+    const statusEndpointBase = "https://api.dune.com/api/v1/execution/";
+    const resultEndpointBase = "https://api.dune.com/api/v1/execution/";
+
+    // Step 1: Trigger an execution through the execute endpoint
+    const executeOptions = {
+        method: 'POST',
+        headers: {
+            'X-DUNE-API-KEY': apiKey,
+            'Content-Type': 'application/json'
+        }
+    };
+
+    let executionId: string | undefined;
+    try {
+        const executeResponse = await fetch(executeEndpoint, executeOptions);
+        if (!executeResponse.ok) {
+            throw new Error(`Failed to execute query: ${executeResponse.statusText}`);
+        }
+        const executeData = await executeResponse.json();
+        executionId = executeData.execution_id;
+        console.log(`Execution for queryId ${queryId} started with ID: ${executionId}`);
+    } catch (error) {
+        throw new Error(`Failed to trigger execution: ${error}`);
+    }
+
+    // Step 2: Poll the status of the execution until it finishes
+    if (!executionId) {
+        throw new Error("Execution ID not found");
+    }
+    const statusEndpoint = `${statusEndpointBase}${executionId}/status`;
+
+    let executionState: string | undefined;
+    while (true) {
+        try {
+            const statusResponse = await fetch(statusEndpoint, {
+                headers: {
+                    'X-DUNE-API-KEY': apiKey
+                }
+            });
+            if (!statusResponse.ok) {
+                throw new Error(`Failed to fetch execution status: ${statusResponse.statusText}`);
+            }
+            const statusData = await statusResponse.json();
+            executionState = statusData.state;
+
+            if (executionState === "QUERY_STATE_COMPLETED") {
+                console.log("Execution completed");
+                break;
+            } else if (executionState === "QUERY_STATE_ERROR") {
+                throw new Error(`Execution failed: ${statusData.error}`);
+            } else if (executionState === "QUERY_STATE_FAILED") {
+                console.log("Query to fetch latest block number failed, so default start block number to be 0.")
+                return 0; 
+            } 
+            else {
+                console.log(`Execution state: ${executionState}`);
+                await new Promise(resolve => setTimeout(resolve, 3000)); // Poll every 3 seconds
+            }
+        } catch (error) {
+            throw new Error(`Error while polling execution status: ${error}`);
+        }
+    }
+
+    // Step 3: Fetch the result of the execution through the result endpoint
+    const resultEndpoint = `${resultEndpointBase}${executionId}/results?limit=1`;
+    try {
+        const resultResponse = await fetch(resultEndpoint, {
+            headers: {
+                'X-DUNE-API-KEY': apiKey
+            }
+        });
+        if (!resultResponse.ok) {
+            throw new Error(`Failed to fetch execution result: ${resultResponse.statusText}`);
+        }
+        const resultData = await resultResponse.json();
+        const result = resultData.result.rows[0]._col0; 
+        return parseInt(result, 10);
+    } catch (error) {
+        throw new Error(`Error while fetching execution result: ${error}`);
+    }
+}
+
+async function archiveQuery(apiKey: string, queryId: number) {
+    const url = `https://api.dune.com/api/v1/query/${queryId}/archive`;
+    const options = {
+        method: 'POST',
+        headers: {
+            'X-DUNE-API-KEY': apiKey,
+            'Content-Type': 'application/json'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            throw new Error(`Failed to archive query: ${response.statusText}`);
+        }
+        console.log(`Successfully archived queryId: ${responseData.query_id}`);
+    } catch (error) {
+        // Handle any errors
+        console.error("Error archiving query:", error);
+        throw error;
+    }
+}
+
+
 async function uploadChunkToDune(chunk: string, apiKey: string, duneTableNamespace: string, duneTableName: string) {
     /*
     This function uploads a chunk of a CSV file to Dune as a table.
-    @TODO: upload only from specific block number? read from dune to get latest block number and start from there?
     */
     const url = `https://api.dune.com/api/v1/table/${duneTableNamespace}/${duneTableName}/insert`;
     const headers = {
@@ -95,7 +235,7 @@ async function uploadChunkToDune(chunk: string, apiKey: string, duneTableNamespa
     }
 }
 
-async function splitCSVIntoChunks(filePath: string, chunkSizeInLines: number, apiKey: string, duneTableNamespace: string, duneTableName: string) {
+async function splitCSVIntoChunks(filePath: string, chunkSizeInLines: number, startBlockNumber: number, apiKey: string, duneTableNamespace: string, duneTableName: string) {
     /*
     This function reads a CSV file and splits it into chunks of a specified size and then uploads each chunk to Dune as a table.
     */
@@ -111,15 +251,24 @@ async function splitCSVIntoChunks(filePath: string, chunkSizeInLines: number, ap
     let isFirstChunk = true;
     let chunkCounter = 1;
     let header: string | null = null;
+    let blockNumberFound = false;
 
     for await (const line of lineReader) {
         if (isFirstChunk) {
             header = line;
             isFirstChunk = false;
+        } else if (!blockNumberFound) {
+            const blockNumber = parseInt(line.split(',')['1']);
+            if (blockNumber > startBlockNumber) {
+                console.log('Start block_number found:', blockNumber);
+                blockNumberFound = true;
+            } else {
+                continue;
+            }
         } else {
             currentChunk.push(line);
         }
-
+                
         if (currentChunk.length >= chunkSizeInLines) {
             const chunkFilename = `test_${chunkCounter}.csv`;
             if (header) {
@@ -145,7 +294,7 @@ async function splitCSVIntoChunks(filePath: string, chunkSizeInLines: number, ap
     fs.writeFileSync(chunkFilename, currentChunk.join('\n'));
     
     // Upload the last chunk to Dune
-    await uploadChunkToDune(currentChunk.join('\n'), apiKey, duneTableNamespace, duneTableName);
+    await uploadChunkToDune(currentChunk.join('\n'), apiKey, duneTableNamespace, duneTableName); 
 
 
 }
@@ -163,9 +312,20 @@ export async function uploadDataToDune(
         const chunkSizeInLines = 200_000;
         const filePath = `./${duneTableName}.csv`;
         console.log('Reading from saved file:', filePath);
-        await splitCSVIntoChunks(filePath, chunkSizeInLines, dune_api_key, duneTableNamespace, duneTableName);
+
+        // Fetch latest block number from Dune table
+        const queryId = await createDuneQuery(dune_api_key, `select max(block_number) from dune.${duneTableNamespace}.${duneTableName}`); 
+        const latestBlockNumber = await fetchDuneQuery(dune_api_key, queryId) || 0
+        console.log('latestBlockNumber:', latestBlockNumber)
+        await archiveQuery(dune_api_key, queryId) 
+
+        // Upload from the latest block number in chunks
+        const startBlockNumber = latestBlockNumber
+        console.log(`Starting to split CSV and upload in chunk from after this block number: ${startBlockNumber} ...`)
+        await splitCSVIntoChunks(filePath, chunkSizeInLines, startBlockNumber, dune_api_key, duneTableNamespace, duneTableName);
+        
 
     } catch (error) {
-        console.error("Error creating table:", error);
+        console.error(error);
     }
 }
